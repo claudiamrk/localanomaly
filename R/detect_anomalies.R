4bfe5f3c7d902dda06ba1867e04234cf9e8d2f6d
Claudia Mark <claudiamark.22@gmail.com>
2026-09-02 10:41:55 +1200

    updated detect_anomalies so it calls the other functions

--- R/detect_anomalies.R
+++ R/detect_anomalies.R
@@ -1,3 +1,24 @@
 detect_anomalies<-function(){
   #this is the main function for detecting the anomalies
+  cleaned_data<-clean_data(data)
+
+  neighbours<-find_neighbours(cleaned_data)
+
+  statistics<-local_statistics(cleaned,data,neighbours)
+
+  scores<-calculate_scores(statistics)
+
+  anomalies<-identify_anomalies(scores)
+
+  classified<-classify_anomalies(anomalies)
+
+  plot<-plot_anomalies(cleaned_data,classified)
+
+  summary<-summarise_anomalies(classified)
+
+  return(list(
+    anomalies=classified,
+    plot=plot,
+    summary=summary
+  ))
 }
